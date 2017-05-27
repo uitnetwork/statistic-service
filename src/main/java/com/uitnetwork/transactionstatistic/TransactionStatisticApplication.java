8ad8cd95a2235160febf11570db2573dcb34c42e
Ninh Doan <uitnetwork@gmail.com>
2017-05-27 14:10:16 +0800

    Init project

---
+++ src/main/java/com/uitnetwork/transactionstatistic/TransactionStatisticApplication.java
@@ -0,0 +1,12 @@
+package com.uitnetwork.transactionstatistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionStatisticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionStatisticApplication.class, args);
+	}
+}
---
+++ src/test/java/com/uitnetwork/transactionstatistic/TransactionStatisticApplicationTests.java
@@ -0,0 +1,16 @@
+package com.uitnetwork.transactionstatistic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TransactionStatisticApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
